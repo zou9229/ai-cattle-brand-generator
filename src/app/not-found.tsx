1d95d50e1cbc4954f0d9fd895ada5bdcb6495e4a
idoubi <me@idoubi.cc>
2025-10-20 12:52:13 +0800

    update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/shared/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import { SmartIcon } from "@/shared/blocks/common/smart-icon";
+
 export default function NotFoundPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
+      <Image src="/logo.png" alt="Logo" width={80} height={80} />
       <h1 className="text-2xl font-normal">Page not found</h1>
+      <Button asChild>
+        <Link href="/" className="mt-4">
+          <SmartIcon name="ArrowLeft" />
+          <span>Back to Home</span>
+        </Link>
+      </Button>
     </div>
   );
 }
